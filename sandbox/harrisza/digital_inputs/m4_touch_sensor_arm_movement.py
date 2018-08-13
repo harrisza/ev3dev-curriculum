1dab76c4a41ac36d01c8026cfe86d2c188257166
Zac Harrison <harrisza@rose-hulman.edu>
2018-08-13 19:55:11 -0400

    m

--- sandbox/harrisza/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/harrisza/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -73,17 +73,16 @@ def arm_calibration(arm_motor, touch_sensor):
     #   Set the arm encoder position to 0 (the last line below is correct to do that, it's new so no bug there)
 
     # Code that attempts to do this task but has MANY bugs (nearly 1 on every line).  Fix them!
-    # arm_motor.run_forever(speed_sp=MAX_SPEED)
-    # while not touch_sensor.is_pressed:
-    #   time.sleep(0.01)
-    #  if touch_sensor.is_pressed:
-    #     break
+    arm_motor.run_forever(speed_sp=MAX_SPEED)
+    while not touch_sensor.is_pressed:
+       time.sleep(0.01)
+
 
     arm_motor.stop(stop_action="brake")
     ev3.Sound.beep().wait()
 
     arm_revolutions_for_full_range = -14.2
-    arm_motor.run_to_rel_pos(position_sp=arm_revolutions_for_full_range, speed_sp=MAX_SPEED)
+    arm_motor.run_to_rel_pos(position_sp=arm_revolutions_for_full_range * 90, speed_sp=MAX_SPEED)
     arm_motor.wait_while(ev3.Motor.STATE_STALLED)
     ev3.Sound.beep().wait()
     arm_motor.position = 0  # Calibrate the down position as 0 (this line is correct as is).
