a554d6cfa6c0b7e24205be3809508fd11a666fe8
Zac Harrison <harrisza@rose-hulman.edu>
2018-08-13 13:31:43 -0400

    m

--- sandbox/harrisza/motors/m5_drive_polygon.py
+++ sandbox/harrisza/motors/m5_drive_polygon.py
@@ -42,7 +42,7 @@ def main():
 
         for k in range(sides):
             robot.drive_inches(edge_length_in, speed_deg_per_second)
-            robot.turn_degrees(turn_amount, speed_deg_per_second)
+            robot.turn_degrees(90, speed_deg_per_second)
 
         # DONE: 3. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
         #   You are done with the Motors unit!
