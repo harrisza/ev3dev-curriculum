c319ae2e4c6277094f95145bce3362c305c8925b
Zac Harrison <harrisza@rose-hulman.edu>
2018-08-13 17:42:45 -0400

    m

--- sandbox/harrisza/digital_inputs/m1_button_states_with_leds.py
+++ sandbox/harrisza/digital_inputs/m1_button_states_with_leds.py
@@ -95,7 +95,7 @@ def main():
 
         # ev3.Leds.all_off()
 
-        # TODO: 4. Implement the down button to change the color of both LEDs.
+        # DONE: 4. Implement the down button to change the color of both LEDs.
         #   The first press to down should make both LEDs GREEN, the next press makes them RED, then AMBER, then off.
         #   If the user presses the down button again, wrap around the list to GREEN and continue as before.
         #   If the user holds down the button, figure out how to make the color change still only happen once.
@@ -134,7 +134,7 @@ def main():
                 times_pressed = times_pressed + 1
                 time.sleep(0.01)
 
-            # TODO: 5. Formally test your work. When you think you have the problem complete run these tests:
+            # DONE: 5. Formally test your work. When you think you have the problem complete run these tests:
             #   Press Left - Green left LED is on (try holding the button down for a few seconds when you to the press)
             #   Press Right - Right right LED is on
             #   Press Up - Both LEDs are off
@@ -146,7 +146,7 @@ def main():
             #   Press Down - Both LEDs are Red (the cycle repeats)
             #   Press Back - Both LEDs turn Green, the robot says Goodbye and the program exits
 
-            # TODO: 6. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
+            # DONE: 6. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
             #
             # Observation you should make, working with buttons as 'states' is functional but usually 'events' work better.
             # Also observe that we don't use the Enter button.  Enter can cause issues since your program is running at the
