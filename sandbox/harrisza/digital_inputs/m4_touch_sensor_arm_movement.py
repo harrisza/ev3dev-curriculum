a97c7d1073d097aa44441327aa48dbdb00a37eb9
Zac Harrison <harrisza@rose-hulman.edu>
2018-08-13 19:38:43 -0400

    m

--- sandbox/harrisza/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/harrisza/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -74,9 +74,9 @@ def arm_calibration(arm_motor, touch_sensor):
 
     # Code that attempts to do this task but has MANY bugs (nearly 1 on every line).  Fix them!
     arm_motor.run_forever(speed_sp=MAX_SPEED)
-    while not touch_sensor:
+    while not touch_sensor.is_pressed:
         time.sleep(0.01)
-        if touch_sensor:
+        if touch_sensor.is_pressed:
             break
     arm_motor.stop(stop_action="brake")
     ev3.Sound.beep().wait()
