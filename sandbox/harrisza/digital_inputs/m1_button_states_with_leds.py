4c74a89de121eeebe3fe07968ab1d419453e7c73
Zac Harrison <harrisza@rose-hulman.edu>
2018-08-13 17:40:17 -0400

    m

--- sandbox/harrisza/digital_inputs/m1_button_states_with_leds.py
+++ sandbox/harrisza/digital_inputs/m1_button_states_with_leds.py
@@ -79,16 +79,19 @@ def main():
             ev3.Sound.speak("Off")
             ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
             ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+            time.sleep(0.01)
         if btn.left:
             print('left')
             ev3.Sound.speak("Green")
             ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
             ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+            time.sleep(0.01)
         if btn.right:
             print('right')
             ev3.Sound.speak("Red")
             ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
             ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
+            time.sleep(0.01)
 
         # ev3.Leds.all_off()
 
@@ -108,25 +111,28 @@ def main():
                 ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
                 ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
                 times_pressed = times_pressed + 1
-                time.sleep(1)
+                time.sleep(0.01)
+        if btn.down:
             if times_pressed == 1:
                 ev3.Sound.speak("Red")
                 ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
                 ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
                 times_pressed = times_pressed + 1
-                time.sleep(1)
+                time.sleep(0.01)
+        if btn.down:
             if times_pressed == 2:
                 ev3.Sound.speak("Amber")
                 ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.AMBER)
                 ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.AMBER)
                 times_pressed = times_pressed + 1
-                time.sleep(1)
+                time.sleep(0.01)
+        if btn.down:
             if times_pressed == 3:
                 ev3.Sound.speak("Off")
                 ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
                 ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
                 times_pressed = times_pressed + 1
-                time.sleep(1)
+                time.sleep(0.01)
 
             # TODO: 5. Formally test your work. When you think you have the problem complete run these tests:
             #   Press Left - Green left LED is on (try holding the button down for a few seconds when you to the press)
