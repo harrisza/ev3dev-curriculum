49604d67f766ee2c3cc3d12854f55afd02b915ee
Zac Harrison <harrisza@rose-hulman.edu>
2018-08-13 19:02:04 -0400

    m

--- sandbox/harrisza/digital_inputs/m3_ir_events_with_the_screen.py
+++ sandbox/harrisza/digital_inputs/m3_ir_events_with_the_screen.py
@@ -77,10 +77,10 @@ def main():
 
     rc1 = ev3.RemoteControl(channel=1)
 
-    rc1.on_red_up = lambda state: handle_red_up_1(state, dc)
-    rc1.on_red_down = lambda state: handle_red_down_1(state, dc)
-    rc1.on_blue_up = lambda state: handle_blue_up_1(state, dc)
-    rc1.on_blue_down = lambda state: handle_blue_down_1(state, dc)
+    rc1.on_red_up = lambda button_state: handle_red_up_1(button_state, dc)
+    rc1.on_red_down = lambda button_state: handle_red_down_1(button_state, dc)
+    rc1.on_blue_up = lambda button_state: handle_blue_up_1(button_state, dc)
+    rc1.on_blue_down = lambda button_state: handle_blue_down_1(button_state, dc)
 
     # DONE: 5. Create remote control objects for channels 2, 3, and 4. Add lambda callbacks for on_red_up to each one:
     #   Channel 2's .on_red_up should call handle_red_up_2 (that exist already) with state and dc as parameters
@@ -91,9 +91,9 @@ def main():
     rc3 = ev3.RemoteControl(channel=3)
     rc4 = ev3.RemoteControl(channel=4)
 
-    rc2.on_red_up = lambda state: handle_red_up_2(state, dc)
-    rc3.on_red_up = lambda state: handle_red_up_3(state, dc)
-    rc4.on_red_up = lambda state: handle_red_up_4(state, dc)
+    rc2.on_red_up = lambda button_state: handle_red_up_2(button_state, dc)
+    rc3.on_red_up = lambda button_state: handle_red_up_3(button_state, dc)
+    rc4.on_red_up = lambda button_state: handle_red_up_4(button_state, dc)
 
     # Buttons on EV3
     btn = ev3.Button()
