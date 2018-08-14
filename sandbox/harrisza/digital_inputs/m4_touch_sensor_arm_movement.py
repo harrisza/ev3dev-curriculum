64e91e115ad2b942c7d12d4c0f8ea4e3856b3be6
Zac Harrison <harrisza@rose-hulman.edu>
2018-08-13 20:38:43 -0400

    m

--- sandbox/harrisza/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/harrisza/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -81,9 +81,9 @@ def arm_calibration(arm_motor, touch_sensor):
         ev3.Sound.beep().wait()
         arm_motor.run_to_rel_pos(position_sp=-0.1 * 360, speed_sp=MAX_SPEED)
         time.sleep(0.1)
-    while not touch_sensor.is_pressed:
-        arm_revolutions_for_full_range = 14.1
-        arm_motor.run_to_rel_pos(position_sp=-arm_revolutions_for_full_range * 360, speed_sp=MAX_SPEED)
+   
+    arm_revolutions_for_full_range = 14.1
+    arm_motor.run_to_abs_pos(position_sp=-arm_revolutions_for_full_range * 360, speed_sp=MAX_SPEED)
     arm_motor.wait_while(ev3.Motor.STATE_STALLED)
     ev3.Sound.beep().wait()
     arm_motor.position = 0  # Calibrate the down position as 0 (this line is correct as is).
